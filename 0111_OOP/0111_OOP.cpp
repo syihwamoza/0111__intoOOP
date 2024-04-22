2f209d892946e10d58a88ca3156148a276a6ece9
syihwamoza <mozakastella26@gmail.com>
2024-04-22 13:48:05 +0700

    menambahkan string nama, int umyr dan string jurusan

--- 0111_OOP/0111_OOP.cpp
+++ 0111_OOP/0111_OOP.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa {
-public: // akses modifier
+class Mahasiswa{
+    public: // akses modifier
+        string nama;
+        int umur;
+        string jurusan;
+
 };
