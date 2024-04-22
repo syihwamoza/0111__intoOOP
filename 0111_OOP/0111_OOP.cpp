809857c24666893d55a2bf26190ea3326f0830e1
syihwamoza <mozakastella26@gmail.com>
2024-04-22 14:33:12 +0700

    memperbarui fungsi main untuk membuat objek Mahasiswa dan Matakuliah, mengatur nilai anggota, dan memanggil fungsi output dan input

--- 0111_OOP/0111_OOP.cpp
+++ 0111_OOP/0111_OOP.cpp
@@ -37,3 +37,17 @@ class Matakuliah {
               cout << "SKS : " << sks << endl; 
           }
 };
+
+int main() {
+    Mahasiswa mhs;
+    Matakuliah mk;
+
+    mhs.nama = "Budi";
+    mhs.umur = 20;
+    mhs.jurusan = "Teknik Informatika";
+    mhs.output();
+
+    mk.input();
+    mk.output();
+
+}
