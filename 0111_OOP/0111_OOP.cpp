aba5aaa7ecf05106b161c51dd42390e67107fbb3
syihwamoza <mozakastella26@gmail.com>
2024-04-22 13:53:38 +0700

    menambahkan informasi output nama,umur,jurusan

--- 0111_OOP/0111_OOP.cpp
+++ 0111_OOP/0111_OOP.cpp
@@ -7,4 +7,9 @@ class Mahasiswa{
         int umur;
         string jurusan;
 
+        void output() {
+            cout << "Nama: " << nama << endl;
+            cout << "Umur:" << umur << endl;
+            cout << "Jurusan:" << jurusan << endl;
+        }
 };
